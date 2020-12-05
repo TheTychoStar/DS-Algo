ae07ba4cf208c3e3b5bd7ed9c5a223d8697429b2
vancouversteven <sli6@hotmail.com>
2020-12-04 21:17:40 -0500

    changeListDemo to DataStructureDemo, remove stackDemo and TreeDemo.

---
+++ CH2/stevenli/HW1/pycode/DataStructureDemo.py
@@ -0,0 +1,42 @@
+from collections import deque
+
+# This is a py class for data structure demo
+
+
+class listObj:
+    def __init__(self):
+        self = []
+
+
+class dequeObj:
+    def __init__(self):
+        self = []
+
+class stackObj:
+    def __init__(self):
+        self = []
+# create a tree class
+class treeObj:
+    def __init__(self, key):
+        self.left=None
+        self.right = None
+        self.val=key
+
+
+
+
+# create a main function to call above method and class
+def main():
+    print("This is main function")
+
+
+
+
+
+
+if __name__=='__main__':
+    main()
+
+
+
+
